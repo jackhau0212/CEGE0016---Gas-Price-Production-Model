0829599325737305498b7f2ed28f6d2ac28b87ae
jackh <jackhau0212@gmail.com>
2022-11-13 18:02:00 +0000

    model changed to building 6 wells in every year

--- model.py
+++ model.py
@@ -3,25 +3,25 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
-number_of_operational_months = 100
-transition_from_hyperbolic_to_exponential = 60
+number_of_operational_months = 12*30
+transition_from_hyperbolic_to_exponential = 200
 
-data_colomns = [x for x in range(0,100+1)]
-data_colomns.insert(0,"Well")
+
 data_list = []
 
-# building 1 well a month
-for i in range(0, 30):
-    well = Well(end_month=number_of_operational_months, transition_month=transition_from_hyperbolic_to_exponential)
-    well_production = well.monthly_volume_production(well_start_month=i)
-    data_list.append(well_production)
+# building 6 wells in every year at the same time
+for i in range(0, 12*6, 12):
+    for j in range(0, 6):
+        well = Well(end_month=number_of_operational_months, transition_month=transition_from_hyperbolic_to_exponential)
+        well_production = well.monthly_volume_production(well_start_month=i)
+        data_list.append(well_production)
 
 
 data = pd.DataFrame(data=data_list)
 print(data)
 
 total_monthly_production = []
-for i in range(0, 100+1):
+for i in range(0, number_of_operational_months+1):
     total_monthly_production.append(data[i].sum())
     
 plt.plot(total_monthly_production)
